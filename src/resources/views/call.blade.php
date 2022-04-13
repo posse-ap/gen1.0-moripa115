aefe3eafb504a7b12b913f74624192cbb13fe3fc
moripa115 <70812517+moripa115@users.noreply.github.com>
2022-04-13 13:31:38 +0900

    ともくんの完成物

---
+++ src/resources/views/call.blade.php
@@ -0,0 +1,13 @@
+<!DOCTYPE html>
+<html lang="ja">
+<head>
+  <meta charset="UTF-8">
+  <meta name="viewport" content="width=device-width, initial-scale=1.0">
+  <meta http-equiv="X-UA-Compatible" content="ie=edge">
+  <title>通話画面</title>
+</head>
+<body>
+  <h1>通話画面です</h1>
+  
+</body>
+</html>
